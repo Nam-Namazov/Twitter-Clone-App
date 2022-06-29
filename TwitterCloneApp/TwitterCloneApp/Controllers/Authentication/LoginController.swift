0cf4f6832f5d6dcb06dcb265fd70d3bf5741d0f3
Namik <namazov306@gmail.com>
2022-06-29 15:02:47 +0300

    Add utilities for textfield,containerview and dividerview, rename folder Utilities, add uiview and textfield password and email, add stackview with both of them, rename image lock, refact

--- TwitterCloneApp/TwitterCloneApp/Controllers/Authentication/LoginController.swift
+++ TwitterCloneApp/TwitterCloneApp/Controllers/Authentication/LoginController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class LoginController: UIViewController {
     // MARK: - Properties
-    
+
     private let logoImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
@@ -18,28 +18,61 @@ final class LoginController: UIViewController {
         return imageView
     }()
 
+    private lazy var emailContainerView: UIView = {
+        let image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
+        let view = Utilities().inputContainerView(withImage: image, textField: emailTextField)
+        return view
+    }()
+
+    private lazy var passwordContainerView: UIView = {
+        let image = #imageLiteral(resourceName: "lock")
+        let view = Utilities().inputContainerView(withImage: image, textField: passwordTextField)
+        return view
+    }()
+
+    private let emailTextField: UITextField = {
+        let textField = Utilities().textField(withPlaceholder: "Email")
+        return textField
+    }()
+
+    private let passwordTextField: UITextField = {
+        let textField = Utilities().textField(withPlaceholder: "Password")
+        textField.isSecureTextEntry = true 
+        return textField
+    }()
+
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
         configureUI()
-        
     }
 
     // MARK: - Selectors
 
     // MARK: - Helpers
-    
+
     private func style() {
         view.backgroundColor = .twitterBlue
         navigationController?.navigationBar.isHidden = true
         navigationController?.navigationBar.barStyle = .black
     }
-    
+
     private func configureUI() {
         view.addSubview(logoImageView)
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
         logoImageView.setDimensions(width: 150, height: 150)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        stack.axis = .vertical
+        stack.spacing = 8
+        
+        view.addSubview(stack)
+        stack.anchor(top: logoImageView.bottomAnchor,
+                     left: view.leftAnchor,
+                     right: view.rightAnchor,
+                     paddingLeft: 16,
+                     paddingRight: 16)
     }
 }
