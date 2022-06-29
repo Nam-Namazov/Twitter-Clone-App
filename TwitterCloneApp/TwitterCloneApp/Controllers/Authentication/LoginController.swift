488d3ade0adab513d705546e6f336287f9f5b6eb
Namik <namazov306@gmail.com>
2022-06-29 15:41:29 +0300

    Refactoring, add loginButton, add signUp button, add attributes for buttons in utilities (for different fonts one with bold)

--- TwitterCloneApp/TwitterCloneApp/Controllers/Authentication/LoginController.swift
+++ TwitterCloneApp/TwitterCloneApp/Controllers/Authentication/LoginController.swift
@@ -40,6 +40,22 @@ final class LoginController: UIViewController {
         textField.isSecureTextEntry = true 
         return textField
     }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.setTitleColor(.twitterBlue, for: .normal)
+        button.backgroundColor = .white
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        return button
+    }()
+    
+    private let dontHaveAccountButton: UIButton = {
+        let button = Utilities().attributedButton("Don't have an account?", " Sign Up")
+        return button
+    }()
 
     // MARK: - Lifecycle
 
@@ -47,9 +63,27 @@ final class LoginController: UIViewController {
         super.viewDidLoad()
         style()
         configureUI()
+        loginButtonAction()
+        dontHaveAccountButtonAction()
     }
 
     // MARK: - Selectors
+
+    private func loginButtonAction() {
+        loginButton.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+    }
+    
+    private func dontHaveAccountButtonAction() {
+        dontHaveAccountButton.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+    }
+
+    @objc private func handleLogin() {
+        print("login tapped")
+    }
+    
+    @objc private func handleShowSignUp() {
+        print("sign up tapped")
+    }
 
     // MARK: - Helpers
 
@@ -64,15 +98,25 @@ final class LoginController: UIViewController {
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
         logoImageView.setDimensions(width: 150, height: 150)
         
-        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,
+                                                   passwordContainerView,
+                                                   loginButton])
         stack.axis = .vertical
-        stack.spacing = 8
+        stack.spacing = 20
+        stack.distribution = .fillEqually
         
         view.addSubview(stack)
         stack.anchor(top: logoImageView.bottomAnchor,
                      left: view.leftAnchor,
                      right: view.rightAnchor,
-                     paddingLeft: 16,
-                     paddingRight: 16)
+                     paddingLeft: 32,
+                     paddingRight: 32)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.anchor(left: view.leftAnchor,
+                                     bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                     right: view.rightAnchor,
+                                     paddingLeft: 40,
+                                     paddingRight: 40)
     }
 }
