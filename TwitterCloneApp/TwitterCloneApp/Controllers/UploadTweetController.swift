45df80ac9ba25b59d278541b6b5774ae0a3ed3e4
Namik <namazov306@gmail.com>
2022-07-06 00:37:32 +0300

    Refactoring, create profileImageView in uploadTweetController, fetch data user url from maintabbar to profileImageView

--- TwitterCloneApp/TwitterCloneApp/Controllers/UploadTweetController.swift
+++ TwitterCloneApp/TwitterCloneApp/Controllers/UploadTweetController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 final class UploadTweetController: UIViewController {
     // MARK: - Properties
-
+    private let user: User
+    
     private lazy var uploadTweetButton: UIButton = {
         let button = UIButton(type: .system)
         button.backgroundColor = .twitterBlue
@@ -21,12 +22,31 @@ final class UploadTweetController: UIViewController {
         button.layer.cornerRadius = 32 / 2
         return button
     }()
+    
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.setDimensions(width: 48, height: 48)
+        imageView.layer.cornerRadius = 48 / 2
+        return imageView
+    }()
 
     // MARK: - Lifecycle
+
+    init (user: User) {
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
+        configureUI()
         setupNavBar()
         tweetUploadButtonTapped()
     }
@@ -46,6 +66,16 @@ final class UploadTweetController: UIViewController {
                                                            target: self,
                                                            action: #selector(handleCancel))
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: uploadTweetButton)
+    }
+    
+    private func configureUI() {
+        view.addSubview(profileImageView)
+        profileImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                                left: view.leftAnchor,
+                                paddingTop: 16,
+                                paddingLeft: 16)
+        
+        profileImageView.sd_setImage(with: user.profileImageUrl, completed: nil)
     }
 
     // MARK: - Selectors
