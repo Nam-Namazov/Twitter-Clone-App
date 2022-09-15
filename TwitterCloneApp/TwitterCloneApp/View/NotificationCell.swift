de3adb7b8d4991f9a16c143f74cdc46aaab88ff2
Namik <namazov306@gmail.com>
2022-09-16 00:06:45 +0300

    Change NotificationController to NotificationTableViewController, add tableview with custom cells profile image and label without fetching data from database, only ui without data and populate this tableview

---
+++ TwitterCloneApp/TwitterCloneApp/View/NotificationCell.swift
@@ -0,0 +1,64 @@
+//
+//  NotificationCell.swift
+//  TwitterCloneApp
+//
+//  Created by Намик on 9/15/22.
+//
+
+import UIKit
+
+final class NotificationCell: UITableViewCell {
+    static let identifier = "NotificationCell"
+    
+    private lazy var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.setDimensions(width: 48, height: 48)
+        imageView.layer.cornerRadius = 48 / 2
+        imageView.backgroundColor = .twitterBlue
+        let tap = UITapGestureRecognizer(target: self,
+                                         action: #selector(handleProfileImageTapped))
+        imageView.addGestureRecognizer(tap)
+        imageView.isUserInteractionEnabled = true
+        return imageView
+    }()
+    
+    private let notificationLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "testing testing"
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle,
+                  reuseIdentifier: String?) {
+        super.init(style: style,
+                   reuseIdentifier: reuseIdentifier)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        let stack = UIStackView(
+            arrangedSubviews: [profileImageView,
+                               notificationLabel]
+        )
+        stack.spacing = 8
+        stack.alignment = .center
+        
+        addSubview(stack)
+        stack.centerY(inView: self,
+                      leftAnchor: leftAnchor,
+                      paddingLeft: 12)
+    }
+    
+    @objc
+    private func handleProfileImageTapped() {
+        
+    }
+}
