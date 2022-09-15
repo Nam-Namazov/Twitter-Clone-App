e39e81c27325cc94f52bc69e72f402829fc0764b
Namik <namazov306@gmail.com>
2022-09-15 23:32:32 +0300

    Create child notifications in firebase database and upload it when someone tapped like button in feet controller, refact

--- TwitterCloneApp/TwitterCloneApp/Controllers/FeedController.swift
+++ TwitterCloneApp/TwitterCloneApp/Controllers/FeedController.swift
@@ -130,6 +130,13 @@ extension FeedController: TweetCellDelegate {
             cell.tweet?.didLike.toggle()
             let likes = tweet.didLike ? tweet.likes - 1 : tweet.likes + 1
             cell.tweet?.likes = likes
+            
+            // upload notification if tweet is being liked
+            guard !tweet.didLike else { return }
+            NotificationService.shared.uploadNotification(
+                type: .like,
+                tweet: tweet
+            )
         }
     }
     
